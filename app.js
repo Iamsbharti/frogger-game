0139a3b4e8b503a49e30666e5b233aa37184a707
Saurabh <saurabhbharti9@gmail.com>
2020-04-24 10:44:05 +0530

    added moveAutologs/cars to right/left & added win loose conditions

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ document.addEventListener('DOMContentLoaded',()=>{
     const squares = document.querySelectorAll('.grid div')
     const timeLeft = document.querySelector('#time-left')
     const resultDisplay = document.querySelector('#result')
+    const startBtn = document.querySelector('#button')
+    const carsLeft = document.querySelector('.cars-left')
+    const carsRight = document.querySelector('.cars-right')
+    const logsLeft = document.querySelector('.log-left')
+    const logsRight = document.querySelector('.log-right')
 
    //initialize game
     let width = 9
-    let currentIndex=76
-    let timeId
-
+    let currentIndex = 76
+    let timerId
+    let currentTime = 20
    //render frong on starting block
     squares[currentIndex].classList.add('frog')
 
@@ -28,14 +33,142 @@ document.addEventListener('DOMContentLoaded',()=>{
                 if(currentIndex % width < width -1) currentIndex +=1
                 break;
             case 40:
-                if(currentIndex + width < width * width) currentIndex +=width
+                if(currentIndex + width < width * width) currentIndex += width
                 break;
         }
 
      //add frong class to new frog's index
         squares[currentIndex].classList.add('frog')
-
+     //check if frog movement has led to win/loose
+        win()
+        loose()
     }
+
+   //move cars
+    function autoMoveCars(){
+        carsLeft.forEach(carLeft => moveCarsLeft(carLeft))
+        carsRight.forEach(carRight => moveCarsRight(carRight))
+    }
+   //move the car left on a time loop
+    function moveCarsLeft(carLeft){
+        switch(true){
+            case carLeft.classList.contains('c1'):
+                carLeft.classList.remove('c1')
+                carLeft.classList.add('c2')
+            break;
+            case carLeft.classList.contains('c2'):
+                carLeft.classList.remove('c2')
+                carLeft.classList.add('c3')
+            break;
+            case carLeft.classList.contains('c3'):
+                carLeft.classList.remove('c3')
+                carLeft.classList.add('c1')
+            break;
+        }
+    }
+  //move the car right on a time loop
+    function moveCarsRight(carRight){
+        switch(true){
+            case carRight.classList.contains('c1'):
+                carRight.classList.remove('c1')
+                carRight.classList.add('c3')
+            break;
+            case carRight.classList.contains('c2'):
+                carRight.classList.remove('c2')
+                carRight.classList.add('c1')
+            break;
+            case carRight.classList.contains('c3'):
+                carRight.classList.remove('c3')
+                carRight.classList.add('c2')
+            break;
+        }
+    }
+
+  //move logs
+    function autoMoveLogs(){
+        logsLeft.forEach(logLeft => moveLogsLeft(logLeft))
+        logsRight.forEach(logRight => moveLogsRight(logRight))
+    }
+  //move logs Left
+    function moveLogsLeft(logLeft){
+        switch(true){
+            case logLeft.classList.contains('l1'):
+                logLeft.classList.remove('l1')
+                logLeft.classList.add('l2')
+                break;
+            case logLeft.classList.contains('l2'):
+                logLeft.classList.remove('l2')
+                logLeft.classList.add('l3')
+                break;
+            case logLeft.classList.contains('l3'):
+                logLeft.classList.remove('l3')
+                logLeft.classList.add('l4')
+                break;
+            case logLeft.classList.contains('l4'):
+                logLeft.classList.remove('l4')
+                logLeft.classList.add('l5')
+                break;
+            case logLeft.classList.contains('l5'):
+                logLeft.classList.remove('l5')
+                logLeft.classList.add('l1')
+                break;
+        }       
+    }
+  //move logs right
+    function moveLogsRight(logRight){
+        switch(true){
+            case logRight.classList.contains('l1'):
+                logRight.classList.remove('l1')
+                logRight.classList.add('l5')
+                break;
+            case logRight.classList.contains('l2'):
+                logRight.classList.remove('l2')
+                logRight.classList.add('l1')
+                break;
+            case logRight.classList.contains('l3'):
+                logRight.classList.remove('l3')
+                logRight.classList.add('l2')
+                break;
+            case logRight.classList.contains('l4'):
+                logRight.classList.remove('l4')
+                logRight.classList.add('l3')
+                break;
+            case logRight.classList.contains('l5'):
+                logRight.classList.remove('l5')
+                logRight.classList.add('l4')
+            
+        }
+    }
+     
+   //Rules to win 
+    function win(){
+        if(squares[4].classList.contains('frog')){
+            resultDisplay.innerHTML='YOU WON!!'
+            squares[currentIndex].classList.remove('frog')
+            clearInterval(timerId)
+            document.removeEventListener('keyup',moveFrog)
+        }
+    }
+   //Rules to loose
+    function loose(){
+        if((currentTime === 0) || (squares[currentIndex].classList.contains('c1'))
+          || (squares[currentIndex].classList.contains('l5'))  
+          ||(squares[currentIndex].classList.contains('l4'))){
+              resultDisplay.innerHTML='YOU LOOSE'
+              squares[currentIndex].classList.remove('frog')
+              clearInterval(timerId)
+              document.removeEventListener('keyup',moveFrog)
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
 })
